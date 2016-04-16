a5412328c0261473700b63a695d08aa052ae4060
2c843ca2-55ab-4f0c-90e6-0bd867db4545 <chenakeshava1@gmail.com>
2016-04-16 21:35:22 +0530

    Initial Commit

---
+++ MVCTreeView/Backup/MVCTreeView/Models/Composite.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+using System.Web.UI;
+using System.Web.Mvc;
+using System.IO;
+
+namespace MVCTreeView.Models
+{
+    public class Composite
+    {
+    }
+
+    public interface IComposite<T>
+    {
+        T Parent { get; }
+        IEnumerable<T> Children { get; }
+    }
+
+
+    public class CompositeThing : IComposite<CompositeThing>
+    {
+
+        public CompositeThing()
+        {
+
+            Children = new List<CompositeThing>();
+
+        }
+        public string Name { get; set; }
+        public CompositeThing Parent { get; set; }
+        public IEnumerable<CompositeThing> Children { get; set; }
+
+    }
+
+    public static class TreeRenderHtmlHelper
+    {
+        public delegate void Func<T>(T value);
+
+        public static void ForEach<T>(this IEnumerable<T> values, Func<T> function)
+        {
+            foreach (T value in values)
+            {
+                function(value);
+            }
+        }
+
+        public static string RenderTree<T>(this HtmlHelper htmlHelper,IEnumerable<T> rootLocations,Func<T, string> locationRenderer)where T : IComposite<T>
+        {
+            return new TreeRenderer<T>(rootLocations, locationRenderer).Render();
+
+        }
+    }
+
+    public class TreeRenderer<T> where T : IComposite<T>
+    {
+        
+
+        private readonly Func<T, string> locationRenderer;
+
+        private readonly IEnumerable<T> rootLocations;
+
+        private HtmlTextWriter writer;
+
+        public TreeRenderer(IEnumerable<T> rootLocations, Func<T, string> locationRenderer)
+        {
+            this.rootLocations = rootLocations;
+            this.locationRenderer = locationRenderer;
+            
+        }
+
+        public string Render()
+        {
+
+            writer = new HtmlTextWriter(new StringWriter());
+
+            RenderLocations(rootLocations);
+
+            return writer.InnerWriter.ToString();
+
+        }
+
+        /// <summary>
+
+        /// Recursively walks the location tree outputting it as hierarchical UL/LI elements
+
+        /// </summary>
+
+        /// <param name="locations"></param>
+
+        private void RenderLocations(IEnumerable<T> locations)
+        {
+
+            if (locations == null) return;
+
+            if (locations.Count() == 0) return;
+
+            InUl(() => locations.ForEach(location => InLi(() =>
+            {
+
+                writer.Write(locationRenderer(location));
+
+                RenderLocations(location.Children);
+
+            })));
+
+        }
+
+
+
+        private void InUl(Action action)
+        {
+
+            writer.WriteLine();
+
+            writer.RenderBeginTag(HtmlTextWriterTag.Ul);
+
+            action();
+
+            writer.RenderEndTag();
+
+            writer.WriteLine();
+
+        }
+
+        private void InLi(Action action)
+        {
+
+            writer.RenderBeginTag(HtmlTextWriterTag.Li);
+
+            action();
+
+            writer.RenderEndTag();
+
+            writer.WriteLine();
+
+        }
+
+    }
+}
---
+++ MVCTreeView/MVCTreeView/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCTreeView.Models;
+
+namespace MVCTreeView.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to ASP.NET MVC!";
+
+            CompositeThing com = new CompositeThing();
+            List<CompositeThing> test = new List<CompositeThing>();
+
+            CompositeThing child1 = new CompositeThing();
+            child1.Name = "C1";
+
+            CompositeThing child2 = new CompositeThing();
+            child2.Name = "C2";
+            test.Add(child1);
+            test.Add(child2);
+
+            com.Children = test;
+            
+
+            CompositeThing Parent = new CompositeThing();
+            Parent.Name = "Main";
+            com.Parent = Parent;
+            return View(com);
+        }
+
+    }
+}
